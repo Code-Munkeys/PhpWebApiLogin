eb3aaec499ca2f17a90a9d51ecf0c6622bd029ec
Franz Ayestaran <franz@ayestaran.co.uk>
2015-06-10 19:56:22 +0100

    Project Added to GitHub

--- WebApi/loginTest.php
+++ WebApi/loginTest.php
@@ -1,3 +1,13 @@
+<!--
+ Developed by Franz Ayestaran on 10/6/15.
+ Copyright (c) 2015 CodeMunkeys All rights reserved.
+ 
+ You may use this code in your own projects and upon doing so, you the programmer are solely
+ responsible for determining it's worthiness for any given application or task. Here clearly
+ states that the code is for learning purposes only and is not guaranteed to conform to any
+ programming style, standard, or be an adequate answer for any given problem.
+-->
+
 <?php
 require('db.php');
 
